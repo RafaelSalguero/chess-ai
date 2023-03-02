fa2cd4c9b653431f52d92dfdac750cce63395432
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2023-03-01 21:03:25 -0700

    functional model lambda

--- ai.py
+++ ai.py
@@ -8,10 +8,10 @@ from train import get_sim_games
 
 import os
 
-from utils import onehot_encode_board
+from utils import cap_histogram, onehot_encode_board
 from view import print_board
 # Disable GPU training:
-# tf.config.set_visible_devices([], 'GPU')
+tf.config.set_visible_devices([], 'GPU')
 
 # Chess board representation will be a 8x8x6 where the 3rd dimension, each one represents a piece type
 # Piece types: 0 Pawn, 1 Knight,  2 Bishop, 3 Rook, 4 Queen, 5 King
@@ -31,7 +31,15 @@ def get_train_data(depth, quiescence_depth, max_iter, size, cache = False):
 
 # data = np.load("train_data/get_sim_games_2_16384.npz")
 print("generating train data")
-(x_train, y_train, ply_train) = get_train_data(10, 5, 100, 500000, True)
+(x_train, y_train, ply_train) = get_train_data(10, 5, 100, 1000000, True)
+
+indices = cap_histogram(y_train, 4000, 5)
+
+print(f"original len: {len(y_train)}")
+x_train = x_train[indices]
+y_train = y_train[indices]
+
+print(f"capped len: {len(y_train)}")
 
 print("generating test data")
 
@@ -41,26 +49,68 @@ print(f"train sample {x_train.shape[0]} {y_train.shape[0]}:")
 
 print("test sample:")
 
+def signed_log(x):
+    return tf.math.log(tf.math.abs(x) + 1) * tf.math.sign(x)
+
+def inv_signed_log(x):
+    return (tf.math.exp(tf.math.abs(x)) - 1) * tf.math.sign(x)
+
 for i in np.random.random_integers(0, x_test.shape[0], 20):
     print_board(x_test[i])
-    print(f"eval: {y_test[i]}")
+    print(f"eval: {y_test[i]} {signed_log(y_test[i])}")
 
-x_test = onehot_encode_board(x_test)
-x_train = onehot_encode_board(x_train)
+model_x_test = onehot_encode_board(x_test)
+model_x_train = onehot_encode_board(x_train)
+model_y_train = y_train
+model_y_test = y_test
 
-model = tf.keras.Sequential([
-    tf.keras.Input(shape=(8,8,8)),
-    tf.keras.layers.Conv2D(8, 3, padding="same", activation="swish"),
-    tf.keras.layers.Conv2D(8, 3, padding="same", activation="swish"),
-    tf.keras.layers.Conv2D(32, 3, padding="same", activation="swish"),
-    tf.keras.layers.Conv2D(16, 5, padding="same", activation="swish"),
-    
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(16, activation="swish", kernel_regularizer=tf.keras.regularizers.l2(0.01)),
-    tf.keras.layers.BatchNormalization(),
+def create_model():
+    inputs = tf.keras.Input(shape=(8,8,8))
+    x = inputs
+    x = signed_log(x)
 
-    tf.keras.layers.Dense(1)
-])
+    x = tf.keras.layers.Conv2D(16, 5, padding="same", activation="relu")(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    x = tf.keras.layers.Conv2D(16, 5, padding="same", activation="relu")(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    x = tf.keras.layers.Conv2D(16, 5, padding="same", activation="relu")(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    x = tf.keras.layers.Conv2D(16, 5, padding="same", activation="relu")(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    x = tf.keras.layers.MaxPooling2D(pool_size=(2,2))(x)
+
+    x = tf.keras.layers.Conv2D(16, 3, padding="same", activation="relu")(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    x = tf.keras.layers.Conv2D(16, 3, padding="same", activation="relu")(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    x = tf.keras.layers.MaxPooling2D(pool_size=(2,2))(x)
+
+    x = tf.keras.layers.Conv2D(8, 3, padding="same", activation="relu")(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    x = tf.keras.layers.Conv2D(8, 3, padding="same", activation="relu")(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    x = tf.keras.layers.Flatten()(x)
+
+    x = tf.keras.layers.Dense(16, activation="relu")(x)    
+
+    x = tf.keras.layers.Dropout(0.2)(x)
+    x = tf.keras.layers.Dense(1)(x)
+
+    x = inv_signed_log(x)
+
+    model = tf.keras.Model(inputs = inputs, outputs = x)
+    return model
+
+
+model = create_model()
 
 
 print(model.summary())
@@ -69,17 +119,16 @@ model.compile(optimizer='adam', loss='mean_squared_error', metrics=[tf.keras.met
 
 print("train size: " + str(x_train.shape[0]))
 
-model.fit(x_train, y_train, validation_data = (x_test, y_test), epochs=100, batch_size=512)
+model.fit(model_x_train, model_y_train, validation_data = (model_x_test, model_y_test), epochs=200, batch_size=64)
 
+model.save("models/alpha_beta_3")
 
-model.evaluate(x_train,  y_train, verbose=2)
-model.evaluate(x_test,  y_test, verbose=2)
+model.evaluate(model_x_train,  model_y_train, verbose=2)
+model.evaluate(model_x_test,  model_y_test, verbose=2)
 
-y_eval = model(x_test)
+y_eval = inv_signed_log(model(model_x_test))
 print("eval sample:")
 for i in np.random.random_integers(0, x_test.shape[0] - 1, 100):
     print_board(x_test[i])
     print(f"test: {y_test[i]} evalBoard: {evalBoard(x_test[i], 1)} value: {y_eval[i]}")
 
-
-model.save("models/alpha_beta_3")
--- utils.py
+++ utils.py
@@ -25,3 +25,44 @@ def shuffle(items):
         i = i - 1
         j = np.random.randint(i)  # 0 <= j <= i-1
         items[j], items[i] = items[i], items[j]
+
+
+@njit
+def cap_histogram(y, cap, avg_size):
+    """
+        Probabilistically caps the count per bin average on the histogram of y,
+        filtering out more frequent values of y
+
+        Returns the picked indices from y
+    """
+    min_y = np.min(y)
+    bins = np.int64(np.max(y) - min_y)
+    (h, bins) = np.histogram(y, bins)
+    probs = np.zeros(len(h))
+    
+    r_avg = 0
+    total_aprox = 0
+    for i in range(0, len(h)):
+        tail_i = i - avg_size
+        tail = 0 if tail_i < 0 else h[tail_i]
+        curr = h[i]
+        r_avg = ((r_avg * avg_size - tail) + curr) / avg_size
+        probs[i] = min(cap /(r_avg + 0.01),1)
+        total_aprox += curr * probs[i]
+
+    indices = np.empty(round(total_aprox), np.int64)
+    out_index = 0
+    y_index = 0
+    while out_index < len(indices):
+        value = y[y_index]
+        bin = round(y[y_index] - min_y)
+        prob = probs[bin]
+        if(np.random.random() < prob):
+            indices[out_index] = y_index
+            out_index += 1
+        
+        y_index = (y_index + 1) % len(y)
+        
+    return indices
+
+        
